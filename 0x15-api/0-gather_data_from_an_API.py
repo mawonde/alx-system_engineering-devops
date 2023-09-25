c5bb6e92976474b848a5949268c1ab635d301103
mawonde <mawondep@gmail.com>
2023-09-25 11:42:22 -0700

    Fix indentation on first file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Script that, uses REST API, for a given employee ID, returns information about  TODO list 
+"""Script that, uses REST API, for a given employee ID, 
+returns information about  TODO list
 """
 import requests
 import sys
@@ -10,39 +11,39 @@ base_url = "https://jsonplaceholder.typicode.com"
 
 
 def get_employee_todo_progress(employee_id):
-  """Takes in employee ID and make requests to API """
-  response = requests.get(f"{base_url}/todos?userId={employee_id}")
-  if response.status_code == 200:
-    todos = response.json()
+    """Takes in employee ID and make requests to API"""
+    response = requests.get(f"{base_url}/todos?userId={employee_id}")
+    if response.status_code == 200:
+        todos = response.json()
 
-    completed_tasks = [task for task in todos if task["completed"]]
+        completed_tasks = [task for task in todos if task["completed"]]
 
-    total_tasks = len(todos)
+        total_tasks = len(todos)
 
-    num_completed_tasks = len(completed_tasks)
+        num_completed_tasks = len(completed_tasks)
 
-    # Make another request to get the employee's name
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-    employee_name = user_data.get("name", "Unknown")
+        # Make another request to get the employee's name
+        user_response = requests.get(f"{base_url}/users/{employee_id}")
+        user_data = user_response.json()
+        employee_name = user_data.get("name", "Unknown")
 
-    # Print the progress information
-    print(
-      f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):"
-    )
-    for task in completed_tasks:
-      print(f"\t{task['title']}")
-  else:
-    print(f"Failed to retrieve data for employee ID {employee_id}.")
+        # Print the progress information
+        print(
+            f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):"
+        )
+        for task in completed_tasks:
+            print(f"\t{task['title']}")
+    else:
+        print(f"Failed to retrieve data for employee ID {employee_id}.")
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: python script.py <employee_id>")
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-  try:
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
-  except ValueError:
-    print("Employee ID must be an integer.")
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
